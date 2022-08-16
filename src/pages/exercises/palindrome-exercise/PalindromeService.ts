41e2c2506360c4c2337769166c459ca2545f3068
Ethan Hanley <ethan@Ethans-MacBook-Pro.local>
2022-08-16 10:26:06 +0100

    palindromes

--- src/pages/exercises/palindrome-exercise/PalindromeService.ts
+++ src/pages/exercises/palindrome-exercise/PalindromeService.ts
@@ -1,19 +1,40 @@
 const data = [
-    "A man, a plan, a canal, Panama",
-    "Gold is where you find it",
-    "If I had a hi-fi",
-    "Fortune favors the prepared mind",
-    "Rats live on no evil star",
-    "There is no abstract living",
-    "Some men interpret nine memos",
-    "Gnorts, Mr Alien"
+	"A man, a plan, a canal, Panama",
+	"Gold is where you find it",
+	"If I had a hi-fi",
+	"Fortune favors the prepared mind",
+	"Rats live on no evil star",
+	"There is no abstract living",
+	"Some men interpret nine memos",
+	"Gnorts, Mr Alien"
 ];
 
-export const solution = () => {
-    let output = ""
-    for(let str of data) {
-        output += `"${str}" might be a palindrome\n`;
-    }
-    return output;
-};
+export const solution = () => buildOutput(data);
 
+const buildOutput =
+	(sentences: string[]) =>
+		sentences
+			.map(sentence => buildOutputLine(sentence))
+			.join('\n');
+
+const buildOutputLine =
+	(sentence: string) =>
+		`"${sentence}" is${isPalindrome(sentence)? "" : " not"} a palindrome.`;
+
+const isPalindrome =
+	(sentence: string) => (
+		(sentence: string) => sentence === reverse(sentence)
+	)(clean(sentence));
+
+const reverse =
+	(original: string) =>
+		original
+			.split('')
+			.reverse()
+			.join('');
+
+const clean =
+	(original: string) =>
+		original
+			.toLowerCase()
+			.replaceAll(/[\W_]/g, "");
